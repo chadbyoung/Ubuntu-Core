b72c73c695ff6493d65975ceec3256592e59b244
chadyoungdell.com <chad.young@dell.com>
2017-08-23 13:52:59 -0500

    removing old files

--- IOT_Apps/nodered.snap/settings/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-echo STARTING NODE-RED - $SNAP/bin/node-red $SNAP/settings.js $SNAP_USER_DATA
-"$SNAP"/bin/node-red -v --settings "$SNAP/settings.js" --userDir "$SNAP_USER_DATA" "$SNAP_USER_DATA/flows.json"
